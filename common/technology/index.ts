7bdfce2a564190d2859d8bcd1e7c345a79c95ed8
zhonglei-c <lei.zhong@target-energysolutions.com>
2024-01-16 15:08:36 +0800

    save

--- common/technology/index.ts
+++ common/technology/index.ts
@@ -6,8 +6,11 @@ export type TTech = {
   name: string,
   price: number,
   buyEffect?: TEffect[],
-  buff?: TEffect[],
+  roundBegin?: TEffect[],
+  endGameEffect?: TEffect[],
   action?: TEffect[],
+  revealEffect?: TEffect[],
+  winConflict?: TEffect[],
 }
 
 export const Technology: TTech[] = [
@@ -15,15 +18,15 @@ export const Technology: TTech[] = [
     id: 'k1',
     name: '火炮',
     price: 1,
-    buff: [{
-      key: EEffect.dao
-    }],
+    revealEffect: [
+      // todo
+    ],
   },
   {
     id: 'k2',
     name: '限制性军事设备',
     price: 4,
-    buff: [{
+    revealEffect: [{
       key: EEffect.constraint,
       con: [{
         key: EConstraint.higherCouncil
@@ -37,10 +40,13 @@ export const Technology: TTech[] = [
     id: 'k3',
     name: '霍尔茨曼引擎',
     price: 6,
-    buff: [{
+    endGameEffect: [{
       //todo End game
       key: EEffect.constraint
     }],
+    roundBegin: [{
+      key: EEffect.drawCard
+    }]
   },
   {
     id: 'k4',
@@ -49,8 +55,7 @@ export const Technology: TTech[] = [
     buyEffect: [{
       key: EEffect.trashCard
     }],
-    buff: [{
-      // todo every reveal turn
+    revealEffect: [{
       key: EEffect.constraint,
       con: [{
         key: EConstraint.cardBuy,
@@ -61,6 +66,39 @@ export const Technology: TTech[] = [
         key: EEffect.trashCard
       }]
     }],
+  },
+  {
+    id: 'k5',
+    name: '引爆设备',
+    price: 3,
+    winConflict: []
+    // todo 
+  },
+  {
+    id: 'k6',
+    name: '运兵船',
+    price: 2,
+  },
+  {
+    id: 'k7',
+    name: '旗舰',
+    price: 8,
+    buyEffect: [{
+      key: EEffect.getVP
+    }],
+    action: []
+    // todo 
+  },
+  {
+    id: 'k8',
+    name: '麝香毒',
+    price: 4,
+    buyEffect: [{
+      key: EEffect.drawYin, number: 2
+    }],
+    endGameEffect: [{
+      key:EEffect.getVP, number: 0.5
+    }]
   },
 
  ]
